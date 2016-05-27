6c65cf7abefe76ba0949693fec1bb75a48ec784c
Kartik Kannapur <kartikkannapur@gmail.com>
2016-05-27 10:22:02 +0530

    feat : 30 Days of Code - Java -  3 Days Complete

--- HackerRank/30_Days_of_Code/src/com/kartik/programs/Day_01.java
+++ HackerRank/30_Days_of_Code/src/com/kartik/programs/Day_01.java
@@ -15,13 +15,20 @@ public class Day_01 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		int i = 4;
+		double d = 4.0;
+		String s = "HackerRank";
+		
 		Scanner scan = new Scanner(System.in);
-		String inputString = scan.nextLine(); 
+		int firstNumber = scan.nextInt();
+		double secondNumber = scan.nextDouble();
+		String tempString = scan.nextLine();
+		String inputString = scan.nextLine();
 		scan.close();
 	      
-		System.out.println("Hello, World.");
-		
-		System.out.println(inputString);
+		System.out.println(i + firstNumber);
+		System.out.println(d + secondNumber);
+		System.out.println(s + inputString.toString());
 		
 
 	}
